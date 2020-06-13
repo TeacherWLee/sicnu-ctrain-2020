8a742756b372fda471eab257f1e6ae3bb12412c0
Li Wei <wli_edu@163.com>
2020-06-13 13:00:57 +0800

    add demo2-chat

---
+++ sicnu-ctrain-2020/demo2-chat/demo2-chat.h
@@ -0,0 +1,32 @@
+﻿
+// demo2-chat.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// Cdemo2chatApp:
+// 有关此类的实现，请参阅 demo2-chat.cpp
+//
+
+class Cdemo2chatApp : public CWinApp
+{
+public:
+	Cdemo2chatApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Cdemo2chatApp theApp;
---
+++ sicnu-ctrain-2020/demo2-chat/demo2-chatDlg.h
@@ -0,0 +1,45 @@
+﻿
+// demo2-chatDlg.h: 头文件
+//
+
+#pragma once
+
+
+// Cdemo2chatDlg 对话框
+class Cdemo2chatDlg : public CDialogEx
+{
+// 构造
+public:
+	Cdemo2chatDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DEMO2CHAT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButtonSend();
+	afx_msg void OnBnClickedButtonAsServ();
+	afx_msg void OnBnClickedButtonAsClient();
+
+private:
+	SOCKET socket_serv;
+	SOCKET socket_client;
+	sockaddr_in si;
+	void SetInfo(CString info);
+	CString strInfo = _T("");
+};
